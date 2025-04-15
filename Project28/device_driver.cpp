c758eefd5de46c9eab4c2bbc9e254d5f03caa90e
leesuji <leesuji98@naver.com>
2025-04-15 09:41:38 +0900

    1st TDD

--- Project28/device_driver.cpp
+++ Project28/device_driver.cpp
@@ -6,8 +6,11 @@ DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    m_hardware->read(address);
+    m_hardware->read(address);
+    m_hardware->read(address);
+    m_hardware->read(address);
+    return m_hardware->read(address);
 }
 
 void DeviceDriver::write(long address, int data)
--- Project28/main.cpp
+++ Project28/main.cpp
@@ -1,14 +1,29 @@
 #include "gmock/gmock.h"
+#include "gtest/gtest.h"
 #include "device_driver.h"
 
-TEST(DeviceDriver, ReadFromHW) {
-	// TODO : replace hardware with a Test Double
-	FlashMemoryDevice* hardware = nullptr;
-	DeviceDriver driver{ hardware };
-	int data = driver.read(0xFF);
-	EXPECT_EQ(0, data);
-}
+class MockFlashMemory : public FlashMemoryDevice {
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+};
 
+TEST(DeviceDriver, ReadFromHW) {
+	MockFlashMemory mock;
+	EXPECT_CALL(mock, read(0xBB)).Times(5);
+
+	DeviceDriver driver{ &mock };
+	driver.read(0xBB);
+}
+/*
+TEST(DeviceDriver, WritetoHW) {
+	MockFlashMemory mock;
+	DeviceDriver driver{ &mock };
+	driver.write(0xFF, 0x00);
+
+	EXPECT_EQ(0, driver.read(0xFF));
+}
+*/
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
